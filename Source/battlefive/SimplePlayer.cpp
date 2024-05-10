57246c1b6a9907943e7d78f7f0b2ae2c6d31febf
octopus7 <octopus7net@gmail.com>
2024-05-11 04:55:45 +0900

    input, camera, move

--- Source/battlefive/SimplePlayer.cpp
+++ Source/battlefive/SimplePlayer.cpp
@@ -15,13 +15,39 @@
 
 DEFINE_LOG_CATEGORY(LogTemplateCharacter);
 
-
-// Sets default values
+// 생성자
 ASimplePlayer::ASimplePlayer()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// 컴포넌트 생성 초기화	
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
+	// 설정
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true; // 인풋 방향으로 이동
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
+
+	// 무브먼트 컴포넌트는 자주 변경하면서 세팅는 특성상 블루 프린트 제어 더 효율적이므로 코드 수정보다는 에디터에서 조정
+	GetCharacterMovement()->JumpZVelocity = 700.f;
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
+
+	// 플레이어 카메라 설정
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 400.0f; // 카메라 암 거리
+	CameraBoom->bUsePawnControlRotation = true; // 컨트롤러 기반으로 암회전
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // 카메라붐 끝에 카메라 부착
+	FollowCamera->bUsePawnControlRotation = false; // 카메라는 암에 대해 상대회전 끔
+
+	// 이거까지 코드로 짜야하나 싶기도 하지만 일단 예제니 가져감
+	// PrimaryActorTick.bCanEverTick = true;
 }
 
 // Called when the game starts or when spawned
@@ -52,8 +78,9 @@ void ASimplePlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 
 	// Set up action bindings
 	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent)) {
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ASimplePlayer::Jump);
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		EnhancedInputComponent->BindAction(AttackAction, ETriggerEvent::Started, this, &ASimplePlayer::Attack);
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ASimplePlayer::Move);
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ASimplePlayer::Look);
 	}
@@ -101,7 +128,8 @@ void ASimplePlayer::Look(const FInputActionValue& Value)
 
 void ASimplePlayer::Jump(const FInputActionValue& Value)
 {
-
+	//ACharacter::Jump();
+	UE_LOG(LogTemplateCharacter, Display, TEXT("'%s' JUMP"), *GetNameSafe(this));
 }
 
 void ASimplePlayer::Lock(const FInputActionValue& Value)
@@ -111,7 +139,7 @@ void ASimplePlayer::Lock(const FInputActionValue& Value)
 
 void ASimplePlayer::Attack(const FInputActionValue& Value)
 {
-
+	UE_LOG(LogTemplateCharacter, Display, TEXT("'%s' ATTACK"), *GetNameSafe(this));
 }
 
 void ASimplePlayer::Fire(const FInputActionValue& Value)
